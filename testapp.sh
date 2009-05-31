3c7303a0177713a8c19ffd9a0fad52df7c76ac82
Andres Aquino <andresaquino@chacharita.(none)>
2009-05-31 00:31:20 -0500

    aplicaciones que cambiaron en plataforma

---
+++ testapp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh 
+# vim: set ts=3 sw=3 sts=3 et si ai: 
+# 
+# testapp.sh -- put here a short description 
+# ----------------------------------------------------------------------------
+
+while true
+do
+   echo "executing...`date`"
+   sleep 5
+done
+
+#
