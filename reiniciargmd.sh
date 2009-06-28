4d2443c5adbfd208553f642b2bcaa4b19f525e9a
Andres Aquino <andres.aquino@gmail.com>
2009-06-28 02:10:26 -0500

    Eliminar archivos que no son utiles para el stable

--- reiniciargmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo " * * * * * * * * * * * * * * * * Bajando Modulos GMD"
-/bscs/bscuat/bin/stop_md ALL
-sleep 5
-
-echo " * * * * * * * * * * * * * * * * Levantando GMD"
-start_gmd
-
