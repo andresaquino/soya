dc963e5598dc249995bcda1958feb757c5edf3fb
Andres Aquino <andres.aquino@gmail.com>
2009-08-03 18:42:37 -0500

    ahora hace un upgrade...

--- install.sh
+++ install.sh
@@ -8,10 +8,12 @@
 # César Andrés Aquino <cesar.aquino@nextel.com.mx>
 
 mkdir -p ~/bin
+# si existe, moverlo
+[ -d ~/soya ] && mv ~/soya ~/soya.old
 [ -d ~/soya.git ] && mv ~/soya.git ~/soya
+[ -d ~/soya.oldi/setup ] && cp -rp ~/soya.old/setup/* ~/soya/setup/ 
 chmod 0750 ~/soya/soya.sh
 ln -sf ~/soya/soya.sh ~/bin/soya
 PATH=$HOME/bin:$PATH
-echo "Set this:\n${PATH}"
 
 #
