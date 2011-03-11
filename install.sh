b3a159102401ed485df64d0d53c94a648d555fd1
Andres Aquino <andres.aquino@gmail.com>
2011-03-11 13:30:20 -0600

    TASK: fix installation process

--- install.sh
+++ install.sh
@@ -1,55 +1,48 @@
 #!/bin/sh 
-# vim: set ts=2 sw=2 sts=2 si ai: 
+# vim: set ts=2 sw=2 sts=2 si ai et: 
 
-# install.sh -- instalar soya en el directorio
+# install.sh 
 # =-=
+#
 # Developer
-# Andres Aquino Morales <andres.aquino@gmail.com>
+# Andres Aquino <aquino@hp.com>
 # 
 
-
+echo "[1] - Creating structure..."
 mkdir -p ~/bin
 mkdir -p ~/manuals/man1
 
-# respaldar setup actual
-cd ~
-echo "Migrando configuraciones"
-[ -d ~/soya ] && cp -rp ~/soya/setup/*.conf ~/soya.git/setup/ 
+echo "[2] - Migrating all config files to new version..."
 cd ~/soya.git/setup/
+[ -d ~/soya ] && cp -rp ~/soya/setup/*.conf .
 for ap in *.conf
 do
-	ln -sf ../soya.sh ${ap%.conf}.start
-	ln -sf ../soya.sh ${ap%.conf}.stop
-	ln -sf ../soya.sh ${ap%.conf}.check
-	ln -sf ../soya.sh ${ap%.conf}.logson
-	ln -sf ../soya.sh ${ap%.conf}.logsoff
-	ln -sf ../soya.sh ${ap%.conf}.syslogson
-	ln -sf ../soya.sh ${ap%.conf}.syslogsoff
-	ln -sf ../soya.sh ${ap%.conf}.dblogson
-	ln -sf ../soya.sh ${ap%.conf}.dblogsoff
+  ln -sf ../soya.sh ${ap%.conf}.start
+  ln -sf ../soya.sh ${ap%.conf}.stop
+  ln -sf ../soya.sh ${ap%.conf}.check
+  ln -sf ../soya.sh ${ap%.conf}.logson
+  ln -sf ../soya.sh ${ap%.conf}.logsoff
+  ln -sf ../soya.sh ${ap%.conf}.syslogson
+  ln -sf ../soya.sh ${ap%.conf}.syslogsoff
+  ln -sf ../soya.sh ${ap%.conf}.dblogson
+  ln -sf ../soya.sh ${ap%.conf}.dblogsoff
 done
 
+echo "[3] - Switching to new version..."
 cd ~
-# mover actual como backup
-echo "Se respaldo la anterior configuracion en $HOME/soya.old"
 [ -d ~/soya.old ] && rm -fr ~/soya.old
 [ -d ~/soya ] && mv ~/soya ~/soya.old
-
-# instalar nuevo componente
 [ -d ~/soya.git ] && mv ~/soya.git ~/soya
 
-# copiar manual
-echo "Siempre podras consultar el manual con: man soya"
+echo "[4] - Installing unix documentation..."
 cp ~/soya/man1/soya.1 ~/manuals/man1/
-
 ln -sf ~/soya/soya.sh ~/soya/soya.version
 ln -sf ~/soya/soyarc ~/.soyarc
 ln -sf ~/soya/screenrc ~/.screenrc
 
+echo "[5] - Fixing permissiont..."
 chmod 0640 ~/soya/install.sh 
 chmod 0750 ~/soya/soya.sh
 
-# establecer nuevo path
-PATH=$HOME/bin:$PATH
-
+echo "[*] - That's all..."
 #
